cfa05fcc194e0548ea7d9760f39511995e8c0c39
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-19 12:24:22 +0300

    Added link to logo

--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import Button from './Button'
-import logo from '../assets/images/logo.png'
+import React from 'react';
+import Button from './Button';
+import logo from '../assets/images/logo.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <>
             <nav className='navbar container pt-3 pb-3 d-flex justify-content-between align-items-center'>
                 <div className="d-flex align-items-center">
-                    <img
-                        src={logo}
-                        alt="Stock Prediction Logo"
-                        className="me-3"
-                        style={{ height: "200px", width: "200px" }}
-                    />
+                    <Link className='navbar-brand textlight' to={'/'}>
+                        <img src={logo} alt="Logo" className="me-3" style={{ height: "200px", width: "200px" }} />
+                    </Link>
                 </div>
                 <div>
                     <Button text='Login' class='btn-outline-info' />
