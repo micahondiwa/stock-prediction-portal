05ca93d8866768789808680fba478b4a36031cc7
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-19 18:23:10 +0300

    Login functionality

--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../AuthProvider";
+import logo from "../assets/images/logo.png"
 
 function Header() {
     const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
@@ -13,22 +14,17 @@ function Header() {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-            <Link className="navbar-brand" to="/">STOCK PREDICTION</Link>
+            <img src={logo} alt="Logo" className="me-2" style={{ height: "200px", width: "200px" }} />
+            <Link className="btn btn-info" to="" >Home</Link>
 
             <div className="ms-auto">
                 {!isLoggedIn ? (
                     <>
-                        <Link className="btn btn-outline-light me-2" to="/login">
-                            Login
-                        </Link>
-                        <Link className="btn btn-info" to="/register">
-                            Register
-                        </Link>
+                        <Link className="btn btn-outline-light me-2" to="/login">Login</Link>
+                        <Link className="btn btn-info" to="/register">Register</Link>
                     </>
                 ) : (
-                    <button className="btn btn-danger" onClick={handleLogout}>
-                        Logout
-                    </button>
+                    <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
                 )}
             </div>
         </nav>
--- frontend-react/src/components/Login.jsx
+++ frontend-react/src/components/Login.jsx
@@ -37,7 +37,7 @@ function Login() {
     };
 
     return (
-        <div className="container">
+        <div className="container flex items-center justify-center flex-1">
             <div className="row justify-content-center">
                 <div className="col-md-6 bg-light-dark p-5 rounded">
                     <h3 className="text-light text-center mb-4">
