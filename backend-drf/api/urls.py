382f5d8a0fd0fe072e95f6f27db76ecd3692116e
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-19 16:10:26 +0300

    Implement simple jwt

--- backend-drf/api/urls.py
+++ backend-drf/api/urls.py
@@ -4,4 +4,10 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', USerViews.RegisterView.as_view()),
+    urlpatterns = [
+    ...
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    ...
+]
 ]
