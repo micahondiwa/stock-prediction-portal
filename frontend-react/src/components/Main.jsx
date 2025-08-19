ddc6a9911e1019e16bcc78e7b0b2508c294ec638
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-19 10:09:14 +0300

    Login button added

--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -7,6 +7,7 @@ const Main = () => {
                 <div className='p-5 text-center bg-light-dark rounded'>
                     <h1 className='text-light'>Stock Prediction Portal</h1>
                     <p className='text-light lead'>Welcome to your intelligent companion for navigating the financial markets. Powered by a robust Django REST Framework (DRF) backend and a sleek React frontend, the platform delivers seamless access to real-time financial data. At its core, it leverages machine learning models and cutting-edge large language models (LLMs) to analyze patterns, interpret market sentiment, and generate accurate price forecasts. With just a few clicks, users can select their preferred stock, explore historical performance, and instantly predict future prices through interactive dashboards. Whether you’re a beginner seeking guidance or an experienced trader looking for deeper insights, this solution combines advanced AI with intuitive design to help you make smarter investment decisions.</p>
+                    <a className='btn btn-outline-info' href=''>Login</a>
                 </div>
             </div>
         </>
