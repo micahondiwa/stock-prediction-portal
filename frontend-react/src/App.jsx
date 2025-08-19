24d1e3af3d124789be84d2b5bb3ccd1f250dedc4
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-19 12:14:52 +0300

    React routes

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -4,16 +4,20 @@ import './assets/css/style.css'
 import Main from './components/Main'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Register from './components/Register'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path='/' element={<Main />} />
           <Route path='/register' element={<Register />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   )
--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -6,7 +6,6 @@ import Footer from './Footer'
 const Main = () => {
     return (
         <>
-            <Header />
             <div className='container'>
                 <div className='p-5 text-center bg-light-dark rounded'>
                     <h1 className='text-light'>STOCK PREDICTION WITH NEURAL NETWORKS & LLMS</h1>
@@ -14,7 +13,6 @@ const Main = () => {
                     <Button text="Login" class='btn-outline-info' />
                 </div>
             </div>
-            <Footer />
         </>
     )
 }
