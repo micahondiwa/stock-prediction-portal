3f016b72edbed53192f413fb027140e0e579a66a
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-19 10:00:55 +0300

    Main

--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Main = () => {
     return (
-        <div className='text-light'>Main</div>
+        <>
+            <div className='container'>
+                <div className='p-5 text-center'>
+                    <h1 className='text-light'>Stock Prediction Portal</h1>
+                    <p className='text-light'>Welcome to your intelligent companion for navigating the financial markets. Powered by a robust Django REST Framework (DRF) backend and a sleek React frontend, the platform delivers seamless access to real-time financial data. At its core, it leverages machine learning models and cutting-edge large language models (LLMs) to analyze patterns, interpret market sentiment, and generate accurate price forecasts. With just a few clicks, users can select their preferred stock, explore historical performance, and instantly predict future prices through interactive dashboards. Whether you’re a beginner seeking guidance or an experienced trader looking for deeper insights, this solution combines advanced AI with intuitive design to help you make smarter investment decisions.</p>
+                </div>
+            </div>
+        </>
     )
 }
 
