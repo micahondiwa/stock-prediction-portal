502a39e4ff81ee62328e350d5b154b5c4738e65a
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-19 12:12:35 +0300

    React routes

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -3,12 +3,18 @@
 import './assets/css/style.css'
 import Main from './components/Main'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Register from './components/Register'
 
 function App() {
 
   return (
     <>
-      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
