39cd1bf48cfe5d8e5b03577c3ffe86b26cfe8776
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-19 10:53:02 +0300

    Logo added

--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Button from './Button'
+import logo from '../assets/images/logo.png'
 
 const Header = () => {
     return (
         <>
-            <nav className='navbar container pt-3 pb-3 align-items-start'>
-                <a className='navbar-brand text-light' href="">Stock Prediction Portal</a>
-
+            <nav className='navbar container pt-3 pb-3 d-flex justify-content-between align-items-center'>
+                <div className="d-flex align-items-center">
+                    <img
+                        src={logo}
+                        alt="Stock Prediction Logo"
+                        className="me-3"
+                        style={{ height: "200px", width: "200px" }}
+                    />
+                </div>
                 <div>
                     <Button text='Login' class='btn-outline-info' />
                     &nbsp;
