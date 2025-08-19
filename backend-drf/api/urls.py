b3a9a29a08349a6c00ac06a116367e65a8d05bd0
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-19 16:09:53 +0300

    Implement simple jwt

--- backend-drf/api/urls.py
+++ backend-drf/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path 
 from accounts import views as USerViews
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', USerViews.RegisterView.as_view()),
