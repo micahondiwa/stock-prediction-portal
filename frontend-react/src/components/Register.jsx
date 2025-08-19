b2d76ae27230cee4a2b40ae01b91c95060b95ca6
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-19 12:44:04 +0300

    Register

--- frontend-react/src/components/Register.jsx
+++ frontend-react/src/components/Register.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-import Header from './Header'
 
 function Register() {
     return (
         <>
+            <div className='container'>
+                <div className='row justify-content-center' >
+                    <div className='col-md-6 bg-light-dark p-5 rounded' >
+                        <h3 className='text-light text-center mb-4' >Create an Account </h3>
+                        <form>
+                            <input type='text' className='form-control mb-3' placeholder='Enter Username' />
+                            <input type='email' className='form-control mb-3' placeholder='Enter Email Address' />
+                            <input type='password' className='form-control mb-5' placeholder='Set Password' />
+                            <button type='submit' className='btn btn-info d-block mx-auto'>Register</button>
+                        </form>
+                    </div>
+
+                </div>
+
+            </div>
         </>
     )
 }
