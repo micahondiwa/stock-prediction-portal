ba404e4d562bc9e9e4ad4d8aef827c46ce756221
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-19 14:00:18 +0300

    Registration succes

--- frontend-react/src/components/Register.jsx
+++ frontend-react/src/components/Register.jsx
@@ -2,21 +2,21 @@ import React, { useState } from "react";
 import axios from 'axios';
 
 function Register() {
-    const [Username, setUsername] = useState('');
+    const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleRegistration = (e) => {
+    const handleRegistration = async (e) => {
         e.preventDefault();
         const userData = {
-            Username, email, password
+            username, email, password
         }
         try {
-            const response = axios.post('http://127.0.0.1:8000/api/v1/register/', userData);
+            const response = await axios.post('http://127.0.0.1:8000/api/v1/register/', userData);
             console.log('response.data==>', response.data);
             console.log('Registration Successful');
         } catch (error) {
-            console.log('Registration error: ', error.response.data)
+            console.error('Registration error: ', error.response.data)
         }
     }
 
@@ -27,7 +27,7 @@ function Register() {
                     <div className='col-md-6 bg-light-dark p-5 rounded' >
                         <h3 className='text-light text-center mb-4' >Create an Account </h3>
                         <form onSubmit={handleRegistration}>
-                            <input type='text' className='form-control mb-3' placeholder='Enter Username' value={Username} onChange={(e) => setUsername(e.target.value)} />
+                            <input type='text' className='form-control mb-3' placeholder='Enter Username' value={username} onChange={(e) => setUsername(e.target.value)} />
                             <input type='email' className='form-control mb-3' placeholder='Enter Email Address' value={email} onChange={(e) => setEmail(e.target.value)} />
                             <input type='password' className='form-control mb-5' placeholder='Set Password' value={password} onChange={(e) => setPassword(e.target.value)} />
                             <button type='submit' className='btn btn-info d-block mx-auto'>Register</button>
