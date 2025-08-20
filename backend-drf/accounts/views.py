fe81a32f48b3f6e750dffe31a46184638d35cc97
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-20 06:55:56 +0300

    Protected view

--- backend-drf/accounts/views.py
+++ backend-drf/accounts/views.py
@@ -3,8 +3,21 @@ from .serializers import UserSerializers
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializers
     permission_classes = [AllowAny]
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated] # This view can be accessed by logged in users only. 
+
+
+    def get(self, request):
+        response = {
+            'status': 'Request was permitted'
+        }
+        return Response(response)
