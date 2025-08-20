a2d3e87035bdda07e278f5ad1e1065c02b939a44
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-20 07:07:01 +0300

    Explore component

--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -10,7 +10,7 @@ const Main = () => {
                 <div className='p-5 text-center bg-light-dark rounded'>
                     <h1 className='text-light'>STOCK PREDICTION WITH NEURAL NETWORKS & LLMS</h1>
                     <p className='text-light lead'>Welcome to your intelligent companion for navigating the financial markets. Powered by a robust Django REST Framework (DRF) backend and a sleek React frontend, the platform delivers seamless access to real-time financial data. At its core, it leverages machine learning models and cutting-edge large language models (LLMs) to analyze patterns, interpret market sentiment, and generate accurate price forecasts. With just a few clicks, users can select their preferred stock, explore historical performance, and instantly predict future prices through interactive dashboards. Whether you’re a beginner seeking guidance or an experienced trader looking for deeper insights, this solution combines advanced AI with intuitive design to help you make smarter investment decisions.</p>
-                    <Button text="Login" class='btn-outline-info' />
+                    <Button text="Explore" class='btn-outline-info' />
                 </div>
             </div>
         </>
