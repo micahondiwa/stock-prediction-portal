ebed9ffaaa4e4f6ff8b056f179a3a172b7b7b145
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-20 08:33:23 +0300

    Request Interceptor

--- frontend-react/src/axiosInstance.js
+++ frontend-react/src/axiosInstance.js
@@ -3,12 +3,14 @@ import axios from "axios";
 const baseURL = import.meta.env.VITE_BACKEND_BASE_API
 const axiosInstance = axios.create({
     baseURL: baseURL,
+    headers: {
+        'Content-Type': 'application/json'
+    }
 });
 
 // Request interceptor
 axiosInstance.interceptors.request.use(
     function (config) {
-        console.log('request==>', config)
         const accessToken = localStorage.getItem('accessToken')
         if (accessToken) {
             config.headers['Authorization'] = `Bearer ${accessToken}`
--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -3,15 +3,10 @@ import React, { useEffect } from 'react';
 import axiosInstance from '../../axiosInstance';
 
 const Dashboard = () => {
-    const accessToken = localStorage.getItem('accessToken');
     useEffect(() => {
         const fetchProtectedData = async () => {
             try {
-                const response = await axiosInstance.get('/protected-view', {
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`
-                    }
-                })
+                const response = await axiosInstance.get('/protected-view')
                 console.log('Success', response.data)
             } catch (error) {
                 console.log('Error fetching data', error)
