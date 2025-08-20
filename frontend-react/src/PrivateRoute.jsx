cae2ec421d38c0c055aeb9c1ac5f73ca083ace8d
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-20 09:00:28 +0300

    Private Route

---
+++ frontend-react/src/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+import { useContext } from "react"
+import AuthProvider from "./AuthProvider"
+
+function PrivateRoute() {
+    return (
+        <div>PrivateRoute</div>
+    )
+}
+
+export default PrivateRoute
