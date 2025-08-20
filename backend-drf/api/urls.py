44f2c59e614b60ddd0fb31a563e6e0f1ebf39709
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-20 06:56:45 +0300

    Protected view

--- backend-drf/api/urls.py
+++ backend-drf/api/urls.py
@@ -4,6 +4,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', USerViews.RegisterView.as_view()),
+
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('protected-view/', USerViews.ProtectedView.as_view())
 ]
