c319e9ef3afdaa9d90abe8dfeb4cfd35fcdf73c8
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-25 10:33:50 +0300

    Serializers

--- backend-drf/api/serializers.py
+++ backend-drf/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
 
 
-class StockPredictionSerializers(serializers.Serializer):
+class StockPredictionSerializer(serializers.Serializer):
     ticker = serializers.CharField(max_length=20)
--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from .serializers import StockPredictionSerializers
 
 # Create your views here.
 class StockPredictionAPIView(APIView):
+    def post(self, request):
+        serializer = 
