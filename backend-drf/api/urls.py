3ab7cb3eaa74ddab69ed2f29ff84c5a93953a6c5
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-25 10:38:22 +0300

    Serializers

--- backend-drf/api/urls.py
+++ backend-drf/api/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('protected-view/', USerViews.ProtectedView.as_view())
+    path('protected-view/', USerViews.ProtectedView.as_view()),
 
     # Prediction API
     path('predict/', StockPredictionAPIView.as_view(), name='stock_prediction')
