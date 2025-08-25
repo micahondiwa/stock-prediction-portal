6eac0db8f674bc46f9d38bb290347d80efe6a8c7
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-25 10:36:44 +0300

    Serializers

--- backend-drf/api/urls.py
+++ backend-drf/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path 
 from accounts import views as USerViews
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from .views import StockPredictionAPIView
 urlpatterns = [
     path('register/', USerViews.RegisterView.as_view()),
 
@@ -9,4 +9,7 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('protected-view/', USerViews.ProtectedView.as_view())
+
+    # Prediction API
+    path('predict/', StockPredictionAPIView.as_view(), name='stock_prediction')
 ]
